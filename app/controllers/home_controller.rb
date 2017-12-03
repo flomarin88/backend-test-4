6ecd30a37345a6304017281432ecad0251d0378c
Florian MARIN <florian.marin@cubber.com>
2017-12-03 13:42:25 +0100

    Welcome message

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-	def index
-		response = Twilio::TwiML::VoiceResponse.new do |r|
-			r.say 'Hello Monkey'
-		end.to_s
-		render xml: response
-	end
-end
