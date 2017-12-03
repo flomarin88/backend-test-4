19a649efd0e8c855a88b62b6bc705cf62d4ea14a
Florian MARIN <florian.marin@cubber.com>
2017-12-03 23:56:15 +0100

    Refactoring

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -8,7 +8,16 @@ class CallsController < ApplicationController
 
 	def create
 		Call.create_or_update(params)
-		render xml: voice_response_success
+		message = nil
+		case params['Digits']
+        when '1'
+    		message = forward_call_message
+        when '2'
+			message = leave_a_voicemail_message
+        else
+			message = welcome_message
+        end
+		render xml: message
 	end
 
 	def events
@@ -23,35 +32,6 @@ class CallsController < ApplicationController
 		head :ok
 	end
 
-	def voice_response_success
-      Twilio::TwiML::VoiceResponse.new do |r|
-        case params['Digits']
-        when '1'
-    		r.say 'J\'appelle votre correspondant', language: 'fr-FR', voice: 'alice'
-			r.dial do |dial|
-    			dial.number('+33770029132', 
-    				status_callback_event: 'initiated ringing answered completed',
-    				status_callback: url_for('/calls/events'),
-    				status_callback_method: 'POST')
-			end
-			r.say "L'appel est terminé. Au revoir.", language: 'fr-FR'
-			r.hangup
-        when '2'
-			r.say 'Merci de laisser un message après le bip sonore.', language: 'fr-FR'
-			r.record(action: url_for('/calls/voicemail_recorded'),
-				method: 'POST',
-				recording_status_callback: url_for('/calls/voicemails'),
-				recording_status_callback_method: 'POST')
-			r.say 'Aucun message enregistré', language: 'fr-FR'
-        else
-			r.say 'Bonjour', language: 'fr-FR'
-			r.gather(timeout: 15, num_digits: 1) do |gather|
-				gather.say "Pour appeler votre correspondant, tapez 1. Pour lui laisser un message, tapez 2.", language: 'fr-FR'
-			end
-        end
-      end.to_s
-    end
-
     def voicemail_recorded
     	response = Twilio::TwiML::VoiceResponse.new do |r|
     		r.say 'Message enregistré. Au revoir', language: 'fr-FR'
@@ -59,4 +39,39 @@ class CallsController < ApplicationController
     	end.to_s
     	render xml: response
     end
+
+    def welcome_message
+    	Twilio::TwiML::VoiceResponse.new do |r|
+	    	r.say 'Bonjour', language: 'fr-FR'
+			r.gather(timeout: 15, num_digits: 1) do |gather|
+				gather.say "Pour appeler votre correspondant, tapez 1. Pour lui laisser un message, tapez 2.", language: 'fr-FR'
+			end
+		end.to_s
+    end
+
+	def forward_call_message
+		Twilio::TwiML::VoiceResponse.new do |r|
+			r.say 'J\'appelle votre correspondant', language: 'fr-FR', voice: 'alice'
+			r.dial do |dial|
+				dial.number('+33770029132', 
+					status_callback_event: 'initiated ringing answered completed',
+					status_callback: url_for('/calls/events'),
+					status_callback_method: 'POST')
+			end
+			r.say "L'appel est terminé. Au revoir.", language: 'fr-FR'
+			r.hangup
+		end.to_s
+    end
+
+    def leave_a_voicemail_message
+		Twilio::TwiML::VoiceResponse.new do |r|
+    		r.say 'Merci de laisser un message après le bip sonore.', language: 'fr-FR'
+			r.record(action: url_for('/calls/voicemail_recorded'),
+				method: 'POST',
+				recording_status_callback: url_for('/calls/voicemails'),
+				recording_status_callback_method: 'POST')
+			r.say 'Aucun message enregistré', language: 'fr-FR'
+		end.to_s
+    end
+
 end
